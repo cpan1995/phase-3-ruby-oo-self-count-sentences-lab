6a04aececde57c9abd3baeb167ca044f754f710c
cpan1995 <cpan1995@gmail.com>
2021-08-10 17:52:30 -0400

    commit tree b5fe2089cf8ab524ee73b9bdaa707b2ad695ef45

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include? "."
   end
 
   def question?
+    self.include? "?"
 
   end
 
   def exclamation?
+    self.include? "!"
 
   end
 
   def count_sentences
-
+    delimiters = ["!", "?", "."]
+    newArray = []
+    self.split(Regexp.union(delimiters)).select do |sentence|
+      unless sentence == ""
+        newArray << sentence
+      end
+    end
+    newArray.length
   end
 end
+
+pp "Hi, my name is Sophie.".sentence?
